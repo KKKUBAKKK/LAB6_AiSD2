545d073e2b728258dda35612dec0639fba460b6e
KKKUBAKKK <kuba.kindracki@gmail.com>
2024-03-28 12:29:53 +0100

    All I did on labs. Stage 1 working but does not reconstruct the path.

--- Odwiedziny/Lab06.cs
+++ Odwiedziny/Lab06.cs
@@ -1,6 +1,7 @@
 using ASD.Graphs;
 using System;
 using System.Collections.Generic;
+using System.Drawing.Drawing2D;
 using System.Linq;
 
 namespace ASD
@@ -14,11 +15,41 @@ namespace ASD
 		/// <param name="target">Wierzchołek docelowy (dom Grzesia).</param>
 		/// <param name="start">Wierzchołek startowy (wejście z lasu).</param>
 		/// <returns>Pierwszy element pary to informacja, czy rozwiązanie istnieje. Drugi element pary, to droga będąca rozwiązaniem: sekwencja odwiedzanych wierzchołków (pierwszy musi być start, ostatni target). W przypadku, gdy nie ma rozwiązania, ma być tablica o długości 0.</returns>
-		public (bool possible, int[] path) Stage1(
-			int n, DiGraph<int> c, Graph<int> g, int target, int start
-		)
+		public (bool possible, int[] path) Stage1(int n, DiGraph<int> c, Graph<int> g, int target, int start)
 		{
-		    return (false, new int[0]);
+			DiGraph<int> layeredGraph = new DiGraph<int>(g.VertexCount * n);
+			bool isReachable = false;
+
+			for (int layer = 0; layer < n; layer++)
+			{
+				foreach (var edge in g.BFS().SearchAll())
+				{
+					if (layer == edge.Weight || c.HasEdge(layer, edge.Weight))
+					{
+						layeredGraph.AddEdge(edge.From + g.VertexCount * layer, edge.To + g.VertexCount * edge.Weight, edge.Weight);
+					}
+				}
+			}
+
+			for (int i = 0; i < n; i++)
+			{
+				foreach (var e in layeredGraph.DFS().SearchFrom(start))
+				{
+					if (e.To == target + i * g.VertexCount)
+					{
+						isReachable = true;
+						break;
+					}
+				}
+				
+				if (isReachable)
+					break;
+			}
+
+			if (!isReachable)
+				return (false, new int[0]);
+
+			return (true, new int[0]);
 		}
 
 		/// <summary>Drugi etap</summary>
@@ -28,9 +59,7 @@ namespace ASD
 		/// <param name="target">Wierzchołek docelowy (dom Grzesia).</param>
 		/// <param name="starts">Wierzchołki startowe (wejścia z lasu).</param>
 		/// <returns>Pierwszy element pary to koszt najlepszego rozwiązania lub null, gdy rozwiązanie nie istnieje. Drugi element pary, tak jak w etapie 1, to droga będąca rozwiązaniem: sekwencja odwiedzanych wierzchołków (pierwszy musi być start, ostatni target). W przypadku, gdy nie ma rozwiązania, ma być tablica o długości 0.</returns>
-		public (int? cost, int[] path) Stage2(
-			int n, DiGraph<int> c, Graph<int> g, int target, int[] starts
-		)
+		public (int? cost, int[] path) Stage2(int n, DiGraph<int> c, Graph<int> g, int target, int[] starts)
 		{
 		    return (null, new int[0]);
 		}
